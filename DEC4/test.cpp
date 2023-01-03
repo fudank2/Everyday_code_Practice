43671b2ae625a53bdb79e40d1e6b2fd79d75a49c
fudan kumar <63598559+fudank2@users.noreply.github.com>
2023-01-03 10:10:54 +0530

    Add files via upload

---
+++ DEC4/test.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define ll              long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define pll_ll          pair<long long,long long>
+#define vi              vector<int>
+#define vll            vector<long long>
+#define si             set<int>
+#define sll             set<ll,ll>
+#define msll            multiset<ll,ll>
+#define usll             unordered_set<ll>
+#define umsll           unordered_multiset<ll,ll>
+#define mii             map<int,int>
+#define mll_ll           map<long long,long long>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e9
+#define n_inf	        -1e9
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define rep(x,a,b)	for(int x = a; x < b; x++) 
+#define per(x,a,b)	for(int x = a; x > b; x--)
+#define yes cout << "YES"<<endl 
+#define no cout << "NO"<<endl
+#define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool isprime(int x){rep(i,2,sqrt(x)+1){if(x%i==0)return false;}return true;}
+int nsum(int x){return(x*(x+1))/2;}
+int fact(int x){if(x<=1)return 1;else return x*fact(x-1);}
+
+void solve()
+{
+    IO
+    rep(i,0,10)
+    {
+        rep(j,0,10)
+        {
+            if(i==0||i==9||j==0||j==9)cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<"\n";
+    }
+}
+
+
+int main()
+{
+    solve();
+    return 0;
+}
+
+
+
+
+
